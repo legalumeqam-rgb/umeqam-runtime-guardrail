ce66bfe87aa780ff8d343553350e2a706f8af556
legalumeqam-rgb <legalumeqam@gmail.com>
2026-03-05 00:37:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uvicorn
 
-# правильный импорт
+# импорт напрямую из файла
 from umeqam_runtime_guardrail.guardrail import UMEQAMGuardrail
 
 
